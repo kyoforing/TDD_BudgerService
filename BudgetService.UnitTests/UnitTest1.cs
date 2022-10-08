7bc0e55e87601f742ad20ff5b87a6e0c25a8d46d
Kyo Cheng <Kyo Cheng>
2022-10-08 15:35:41 +0800

    init commit

---
+++ BudgetService.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace BudgetService.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ BudgetService/BudgetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BudgetService
+{
+    public class BudgetService
+    {
+        private readonly IBudgetRepo _budgetRepo;
+
+        public BudgetService(IBudgetRepo budgetRepo)
+        {
+            _budgetRepo = budgetRepo;
+        }
+
+        public decimal Query(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
